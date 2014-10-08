32458fe55c420f913f19fc0c41625fec190c5272
Lucas Garron <code@garron.net>
2014-10-08 00:03:32 -0700

    Initial version.

---
+++ mixed-content-test.py
@@ -0,0 +1,36 @@
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+from google.appengine.api import app_identity
+
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+
+        server_url = app_identity.get_default_version_hostname()
+
+        template_values = {
+            "host": server_url
+        }
+
+        if os.environ['HTTP_HOST'].startswith("hsts."):
+            self.response.headers['Strict-Transport-Security'] = 'max-age=300'
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
